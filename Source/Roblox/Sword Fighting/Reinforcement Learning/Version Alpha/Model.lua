755cf88e5fb28ff032498930139950ace72e2e6d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-11-21 22:33:48 +0800

    Update Model.lua

--- Source/Roblox/Sword Fighting/Reinforcement Learning/Version Alpha/Model.lua
+++ Source/Roblox/Sword Fighting/Reinforcement Learning/Version Alpha/Model.lua
@@ -2,7 +2,7 @@ local function buildModel()
 	
 	local ExperienceReplay = DataPredict.ExperienceReplays.UniformExperienceReplay.new(1, 10)
 	
-	local Model = DataPredict.Models.DoubleQLearningNeuralNetworkV2.new(nil, nil, nil, 1000, nil, 0.9999999)
+	local Model = DataPredict.Models.DoubleQLearningNeuralNetworkV2.new(nil, 0.01, nil, 1000, nil, 0.9999999)
 
 	Model:addLayer(15, true, 'Tanh')
 
